45f8646068a363aad3319eb4f532b9d740af13f4
Mario Ponce Júnior <mariojrponce@gmail.com>
2020-08-12 19:57:40 -0300

    First commit

---
+++ database/migrations/2020_08_12_222930_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->text('texto')->nullable();
+            $table->text('descricao')->nullable();
+            $table->integer('num_acesso')->default(0);
+            $table->timestamps();//teste
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
